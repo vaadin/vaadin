3cf4a2d8dba2f60a16964dc54e573c8e86e539df
Serhii Kulykov <iamkulykov@gmail.com>
2026-02-03 11:49:27 +0200

    fix: remove cookie-consent import, add dashboard (#84)

--- vaadin.js
+++ vaadin.js
@@ -1,8 +1,8 @@
 import '@vaadin/vaadin-core';
 import '@vaadin/board';
 import '@vaadin/charts';
-import '@vaadin/cookie-consent';
 import '@vaadin/crud';
+import '@vaadin/dashboard';
 import '@vaadin/grid-pro';
 import '@vaadin/grid-pro/vaadin-grid-pro-edit-column.js';
 import '@vaadin/rich-text-editor';
